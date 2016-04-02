68ede11e08f6fd801cadfeafb4ca4ffcb15b439e
Imed-Eddine-Nezzar <imad.eddine.nezzar@gmail.com>
2016-04-02 14:55:44 +0100

    Improved solution

--- misc/string_array_to_linked.c
+++ misc/string_array_to_linked.c
@@ -28,11 +28,16 @@ void append(Node** list, const char* string) {
     *list = tmp;
 }
 
+void destroy_list(Node* list) {
+    if (list != NULL && list->next == NULL)
+        destroy_node(list);
+    else destroy_list(list->next);
+}
+
 Node* as_linked(char** arr, int size) {
     Node* list = NULL;
-    int i = size - 1;
     while (size--)
-        append(&list, arr[i-size]);
+        append(&list, arr[size]);
 
     return list;
 }
@@ -45,9 +50,10 @@ void print_list(const Node* list) {
 }
 
 int main() {
-    int n = 3;
-    char* strings[] = {"hello", "world", "new"};
-    Node* list = as_linked(strings, n);
+    int size = 5;
+    char* strings[] = {"hello", "world", "happy", "birth", "day"};
+    Node* list = as_linked(strings, size);
     print_list(list);
+    destroy_list(list);
     return 0;
 }
